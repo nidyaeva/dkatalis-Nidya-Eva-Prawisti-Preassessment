ce1397c2f04b28becb4b7220332d3a643f2bddb0
nidyaeva <57339759+nidyaeva@users.noreply.github.com>
2019-11-04 13:54:54 +0700

    Create ReactProject1.js

---
+++ ReactProject1.js
@@ -0,0 +1,10 @@
+const MyComponent = function() {
+  // change code below this line
+  return (
+    <div>
+     This is Demo version
+    </div>
+  );
+ }
+
+ReactDOM.render(<MyComponent />, document.getElementById('root'))
